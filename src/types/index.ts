27673f2719657fcbef35c9a06f62c87e2fd7f651
Teewhy <tyinter46@gmail.com>
2024-06-10 06:56:17 -0700

    feat: building profileViewMoadal

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,7 +7,31 @@ export interface UserDetails {
   stateOfOrigin: string;
   lgOgOrigin: string;
   ward: string;
-  qualifications: string[];
+  qualifications: Qualifications[];
+  dateOfPresentSchoolPosting: string;
+  cadre: string;
+  // dateOfFirstAppointment?: Date;
+  // dateOfLastPromotion?: Date;
+  // dateOfBirth?: Date;
+  gradeLevel: number;
+  pfa: string;
+  pensionNumber: string;
+  // dateOfRetirement?: Date;
+  professionalStatus: string;
+  email: string;
+  ogNumber: string;
+  password: string;
+  confirmationCode: string;
+  profilePhoto: string;
+  tetiaryCertificate: string;
+  primarySchoolCertificate?: string;
+  secondarySchoolCert?: string;
+  firstAppointmentLetter?: string;
+  lastPromotionLetter?: string;
+  birthCertificate?: string;
+  serviceStatus?: string;
+  staffType?: string;
+  // remark?: string;
 }
 
 // export interface UserDetails {
@@ -23,5 +47,13 @@ export interface UserDetails {
 //     state: string;
 //     profilePhoto: string;
 //   }
+
+export interface Qualifications {
+  degreeType: string;
+  specialization: string;
+  startYear: string;
+  endYear: string;
+  schoolName: string;
+}
 
 export interface ExistingStaffDetails {}
